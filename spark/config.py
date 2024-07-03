a78e22d8ec185e9aab7304709a523a7489f4d7e8
nacho dorado <natxo98@gmail.com>
2024-07-03 16:31:42 +0200

    Creacion de conexion entre Spark y Kafka para obtener los datos en streaming

--- spark/config.py
+++ spark/config.py
@@ -1,3 +1,4 @@
+# POSTGRES
 BBDD="bbdd_usuarios"
 TABLA="usuarios"
 HOST="postgres"
@@ -6,3 +7,9 @@ CONTRASENA="airflow"
 PUERTO=5432
 JDBC_URL=f"jdbc:postgresql://{HOST}:{PUERTO}/{BBDD}"
 PROPIEDADES={"user":USUARIO, "password":CONTRASENA, "driver":"org.postgresql.Driver"}
+
+# KAFKA
+HOST_KAFKA="kafka"
+PUERTO_KAFKA="19092"
+SERVIDOR_KAFKA=f"{HOST_KAFKA}:{PUERTO_KAFKA}"
+TOPIC="usuarios"
--- spark/spark_script.py
+++ spark/spark_script.py
@@ -1,30 +1,27 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from typing import Optional
+import sys
 
-from config import JDBC_URL, PROPIEDADES, TABLA
+from config import JDBC_URL, PROPIEDADES, TABLA, SERVIDOR_KAFKA, TOPIC
 
-def crearSesion()->SparkSession:
+def crearSesion()->Optional[SparkSession]:
+
+	paquetes="org.postgresql:postgresql:42.2.23,org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
 	try:
 
 		return SparkSession.builder\
 						.appName("SparkStreaming")\
-						.config("spark.jars.packages", "org.postgresql:postgresql:42.2.23")\
+						.config("spark.jars.packages", paquetes)\
 						.getOrCreate()
 
 	except Exception:
 
-		print("Error al crear la sesion")
+		print("Error al crear la sesion de Spark")
+		sys.exit()
 
-def crearDataframe(spark:SparkSession)->DataFrame:
-
-	data=[(1, "Nacho"),(2, "Amanda")]
-
-	esquema=StructType([StructField("Id", IntegerType(), False), StructField("Nombre", StringType(), False)])
-
-	return spark.createDataFrame(data, schema=esquema)
-
-def leerTabla(tabla:str)->DataFrame:
+def leerTabla(spark:SparkSession, tabla:str)->DataFrame:
 
 	return spark.read.jdbc(url=JDBC_URL, table=tabla, properties=PROPIEDADES)
 
@@ -32,22 +29,42 @@ def escribirTabla(df:DataFrame, tabla:str, modo:str="overwrite")->None:
 
 	df.write.mode(modo).jdbc(url=JDBC_URL, table=tabla, properties=PROPIEDADES)
 
-try:
+def conectarStreamKafka(spark:SparkSession)->Optional[DataFrame]:
+
+	try:
+
+		return spark.readStream\
+					.format("kafka")\
+					.option("kafka.bootstrap.servers", SERVIDOR_KAFKA)\
+					.option("subscribe", TOPIC)\
+					.option("startingOffsets", "earliest")\
+					.load()
+
+	except Exception as e:
+
+		print("Error al conectarse a Kafka: {e}")
+		sys.exit()
+
+def dataframe_seleccionado(df:DataFrame)->DataFrame:
+
+	esquema=StructType([StructField("key", StringType(), True), StructField("value", StringType(), True)])
+
+	return df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+
+if __name__ == "__main__":
 
 	spark=crearSesion()
 
-	df=crearDataframe(spark)
+	df=conectarStreamKafka(spark)
 
-	escribirTabla(df, TABLA)
+	df_seleccionado=dataframe_seleccionado(df)
 
-	df_tabla=leerTabla(TABLA)
+	print("La sesion en stream se esta iniciando...")
 
-	df_tabla.show()
+	consulta_stream=df_seleccionado.writeStream \
+								    .outputMode("append") \
+								    .format("console") \
+								    .start()
 
-except Exception:
-
-	print("Error en la ejecucion")
-
-finally:
-
-	spark.stop()
+	consulta_stream.awaitTermination()
